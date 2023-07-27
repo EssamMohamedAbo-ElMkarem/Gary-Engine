36d948e748f7c960d6e2a3661a2e5f74dbe86556
Essam Mohamed <50668640+EssamMohamedAbo-ElMkarem@users.noreply.github.com>
2023-07-27 11:57:37 +0300

    Add files via upload

---
+++ garySE package/__init__.py
@@ -0,0 +1,9 @@
+# Gary YOLO 🚀, GPL-3.0 license
+
+__version__ = "8.0.0"
+
+#from Gary.hub import checks
+#from Gary.yolo.engine.model import YOLO
+#from Gary.yolo.utils import ops
+
+#__all__ = ["__version__", "YOLO", "hub", "checks"]  # allow simpler import
